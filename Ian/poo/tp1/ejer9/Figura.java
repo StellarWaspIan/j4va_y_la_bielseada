0bb56abd8e0f8aa2e2f64a3e4fb1c38568e3a77f
StellarWasp-ian <avilaian2officialac@gmail.com>
2025-09-07 15:25:50 -0300

    Correcciones a clase Figura y creacion de clase Rectangulo

--- Ian/poo/tp1/ejer9/Figura.java
+++ Ian/poo/tp1/ejer9/Figura.java
@@ -1,11 +1,19 @@
 package Ian.poo.tp1.ejer9;
 
-public interface Figura{
-    double altura=0;
-    double largo=0;
-    String colorfondo="";
-    String colorborde="";
-    double pi=3.14;
+public abstract class Figura{
+    static double altura=0;
+    static double largo=0;
+    static String colorfondo="";
+    static String colorborde="";
+    
+    public Figura(double altura, double largo, String colorfondo, String colorborde) {
+        Figura.altura = altura;
+        Figura.largo = largo;
+        Figura.colorfondo = colorfondo;
+        Figura.colorborde = colorborde;
+    }
 
-    abstract String toString();
+    final double pi=3.14;
+
+    public abstract String toString();
 }
---
+++ Ian/poo/tp1/ejer9/Rectangulo.java
@@ -0,0 +1,12 @@
+package Ian.poo.tp1.ejer9;
+
+public class Rectangulo extends Figura {
+    public Rectangulo(){
+        super(altura, largo, colorfondo, colorborde);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}

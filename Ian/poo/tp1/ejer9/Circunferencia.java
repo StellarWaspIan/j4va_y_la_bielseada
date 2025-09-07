fe7c97da25245188da79a18af4ab4cbe8fad02e7
StellarWasp-ian <avilaian2officialac@gmail.com>
2025-09-07 15:29:09 -0300

    Creacion de clases Circuferencia y Linea, se agrego el pdf del trabajo practico.

---
+++ Ian/poo/tp1/ejer9/Circunferencia.java
@@ -0,0 +1,14 @@
+package Ian.poo.tp1.ejer9;
+
+public class Circunferencia extends Figura {
+
+    public Circunferencia(double altura, double largo, String colorfondo, String colorborde) {
+        super(altura, largo, colorfondo, colorborde);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+    
+}
---
+++ Ian/poo/tp1/ejer9/Linea.java
@@ -0,0 +1,14 @@
+package Ian.poo.tp1.ejer9;
+
+public class Linea extends Figura {
+
+    public Linea(double altura, double largo, String colorfondo, String colorborde) {
+        super(altura, largo, colorfondo, colorborde);
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+    
+}

9954692457bfe86416d3621a47d366bb82677fb1
StellarWasp-ian <avilaian2officialac@gmail.com>
2025-09-07 15:16:34 -0300

    Clase Figura

--- Ian/poo/tp1/ejer9/Figura.java
+++ Ian/poo/tp1/ejer9/Figura.java
@@ -1,5 +1,11 @@
 package Ian.poo.tp1.ejer9;
 
 public interface Figura{
+    double altura=0;
+    double largo=0;
+    String colorfondo="";
+    String colorborde="";
+    double pi=3.14;
 
+    abstract String toString();
 }
